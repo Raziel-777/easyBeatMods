d51b0cd67aff9d59e13210d198573b6db081351d
Damien THIBAULT <damien.thibault@ponant-technologies.com>
2025-01-02 11:25:26 +0100

    new beatsmods api

--- main.py
+++ main.py
@@ -5,60 +5,76 @@ import zipfile
 
 import requests
 
+PUBLIC_CDN_BASE_URL = "https://bbm.saera.gay/cdn"
 BASE_URL = 'https://beatmods.com'
-GAME_VERSION = '1.40'
-API_URL = f'/api/v1/mod?status=approved&gameVersion={GAME_VERSION}'
+GAME_VERSION = '1.40.0'
+API_URL = f'/api/mods?gameName=BeatSaber&gameVersion={GAME_VERSION}&status=verified&platform=universalpc'
 MODS_URL = f"{BASE_URL}{API_URL}"
-MODS = ['BSIPA', 'SongCore', 'BetterPause', 'Custom Platforms', '_Heck', 'Chroma', 'BetterSongList',
-        'BetterSongSearch', 'NoodleExtensions', 'PlaylistManager', 'SongPlayHistory', 'CustomSabersLite',
-        'Custom Avatars', 'Camera2']
+MOD_URL = f"{BASE_URL}/api/mods/"
+MOD_VERSION_URL = f"{BASE_URL}/api/modversions/"
+MODS = ['bsipa', 'songcore', 'betterpause', 'heck', 'chroma', 'bettersonglist', 'bettersongsearch',
+        'noodleextensions', 'playlistmanager', 'songplayhistory', 'customsaberslite', 'custom avatars', 'camera2']
 MODS_DIRECTORY = './mods'
 ARCHIVE = './mods_archive'
+ALREADY_DOWNLOADED = []
+ALREADY_DOWNLOADED_WITH_VERSION = []
+SKIP_VERIFIED = True
 
 
-def download_mod(url, mod_dir_path, temp_dir):
-    mod_resp = requests.get(url, stream=True)
-    mod_resp.raise_for_status()
-    zip_path = os.path.join(temp_dir, "archive_temp.zip")
-    with open(zip_path, "wb") as file:
-        for chunk in mod_resp.iter_content(chunk_size=8192):
-            file.write(chunk)
-    with zipfile.ZipFile(zip_path, mode="r") as zipf:
-        zipf.extractall(mod_dir_path)
-    os.remove(zip_path)
+def download_mod(mod_name, mod_version, mod_dir_path, temp_dir):
+    if mod_name is None:
+        mod_id = mod_version['modId']
+        URL = f"{MOD_URL}{mod_id}"
+        response = requests.get(f"{URL}")
+        response.raise_for_status()
+        mod_name = response.json()['mod']['info']['name'].lower()
+        print(f'dep -------> {mod_name}')
+    if mod_name not in ALREADY_DOWNLOADED:
+        zip_hash = mod_version["zipHash"]
+        zip_url = f'{PUBLIC_CDN_BASE_URL}/mod/{zip_hash}.zip'
+        mod_resp = requests.get(zip_url, stream=True)
+        mod_resp.raise_for_status()
+        zip_path = os.path.join(temp_dir, "archive_temp.zip")
+        with open(zip_path, "wb") as file:
+            for chunk in mod_resp.iter_content(chunk_size=8192):
+                file.write(chunk)
+        with zipfile.ZipFile(zip_path, mode="r") as zipf:
+            zipf.extractall(mod_dir_path)
+        os.remove(zip_path)
+        ALREADY_DOWNLOADED.append(mod_name)
+        ALREADY_DOWNLOADED_WITH_VERSION.append(f'{mod_name} {mod_version["modVersion"]}')
 
 
 if __name__ == '__main__':
-    already_downloaded = []
-    already_downloaded_with_version = []
     if os.path.exists(MODS_DIRECTORY):
         shutil.rmtree(MODS_DIRECTORY)
     os.makedirs(MODS_DIRECTORY)
 
     with tempfile.TemporaryDirectory() as temp_dir:
-        for mod in MODS:
-            URL = f"{MODS_URL}&name={mod}&sort=version&sortDirection=-1"
-            response = requests.get(f"{URL}")
-            response.raise_for_status()
-            versions = response.json()
-            if versions:
-                latest_version = versions[0]
-                for download in latest_version['downloads']:
-                    if download['type'] == 'universal':
-                        download_mod(f'{BASE_URL}{download["url"]}', MODS_DIRECTORY, temp_dir)
-                        already_downloaded.append(mod)
-                        already_downloaded_with_version.append(f'{mod} {latest_version["version"]}')
-                    else:
-                        raise Exception(f'not universal {mod}')
-                for dependency in latest_version['dependencies']:
-                    if dependency['name'] not in already_downloaded:
-                        for download in dependency['downloads']:
-                            if download['type'] == 'universal':
-                                download_mod(f'{BASE_URL}{download["url"]}', MODS_DIRECTORY, temp_dir)
-                                already_downloaded.append(dependency['name'])
-                                already_downloaded_with_version.append(f'{dependency["name"]} {dependency["version"]}')
-                            else:
-                                raise Exception(f'not universal {dependency["name"]}')
+        all_mods_resp = requests.get(f"{MODS_URL}")
+        all_mods_resp.raise_for_status()
+        all_mods = all_mods_resp.json()['mods']
+        MODS_LOWER = [mod.lower() for mod in MODS]
+        for mod in all_mods:
+            name = mod['mod']['name'].lower()
+            mod_id = mod['mod']['id']
+            if name in MODS_LOWER and name not in ALREADY_DOWNLOADED:
+                print(name)
+                mod_url = f"{MOD_URL}{mod_id}"
+                response = requests.get(f"{mod_url}")
+                response.raise_for_status()
+                mod_versions = response.json()['mod']['versions']
+                for mod_version in mod_versions[::-1]:
+                    if SKIP_VERIFIED or mod_version['status'] == 'verified':
+                        download_mod(name, mod_version, MODS_DIRECTORY, temp_dir)
+                        for dependency in mod_version['dependencies']:
+                            dependency_url = f"{MOD_VERSION_URL}{dependency}?&raw=true"
+                            resp = requests.get(f"{dependency_url}")
+                            resp.raise_for_status()
+                            dependency_mod_version = resp.json()['modVersion']
+                            download_mod(None, dependency_mod_version, MODS_DIRECTORY, temp_dir)
+                        break
+
     shutil.make_archive(ARCHIVE, 'zip', MODS_DIRECTORY)
-    for el in already_downloaded_with_version:
+    for el in ALREADY_DOWNLOADED_WITH_VERSION:
         print(el)
